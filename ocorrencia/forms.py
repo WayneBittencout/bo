a5ed9c7f65d8082320348c47ee0f91c743303677
Helder Cruz <hdr.cruz@gmail.com>
2022-05-05 14:58:54 -0300

    mudar widget

--- ocorrencia/forms.py
+++ ocorrencia/forms.py
@@ -1,6 +1,9 @@
-from django.forms import ModelForm
-from .models import Bo
+from django.forms import ModelForm, ModelMultipleChoiceField, CheckboxSelectMultiple
+from .models import Bo, Natureza
+
 class PostForm(ModelForm):
+    natureza_crime = ModelMultipleChoiceField(queryset=Natureza.objects.all(), widget=CheckboxSelectMultiple)
+
     class Meta:
         model = Bo
         fields = ['numero','data_fato','data_registro','local_registro','meios_empregados','natureza_crime','situacao_bo','obs']
