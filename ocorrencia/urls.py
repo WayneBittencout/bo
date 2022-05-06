6e26b8b0daf2cf055ecc09744f95476fd723ef73
Helder Cruz <hdr.cruz@gmail.com>
2022-05-06 13:29:09 -0300

    criar url para lista de bo

--- ocorrencia/urls.py
+++ ocorrencia/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('<int:ano>', views.index, name='index'),
     path('', views.index, name='index'),
     path('cadastro/', views.cadastro, name='cadastro'),
+    path('lista/', views.BoLista.as_view(), name='lista'),
     path('editar/<int:pk>', views.BoAtualizar.as_view(), name='editar'),
     path('envolvido/', views.cadastro, name='envolvido'),
     path('alerta/', views.alertas, name='alerta'),
